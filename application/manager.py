5467e1be697558afafc4c3135bc55ac998017506
akhilaryan <akhil.ion@gmail.com>
2014-08-23 14:30:15 +0100

    Basics done. now to work towards the actual counting.

--- application/manager.py
+++ application/manager.py
@@ -6,10 +6,13 @@ from application.models import *
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('info/index.html', title='Flask-Bootstrap')
+    return render_template('info/index.html', title='Click Counter')
 
+@app.route('/count')
+def count():
+	return render_template('info/count.html', title = 'Click count')
 
-@app.route('/hello/<username>/')
-def hello_username(username):
-    return render_template('info/hello.html', title="Flask-Bootstrap, Hi %s"
-                            % (username), username=username)
+# @app.route('/hello/<username>/')
+# def hello_username(username):
+#     return render_template('info/hello.html', title="Flask-Bootstrap, Hi %s"
+#                             % (username), username=username)
--- application/models.py
+++ application/models.py
@@ -5,11 +5,11 @@ from application import db
 # use a model per blueprint.
 
 ##
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True)
-#     email = db.Column(db.String(80), unique=True)
-#     password = db.Column(db.String(80))
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
 
 #     def __init__(self, username, email, password):
 #         self.username = username
